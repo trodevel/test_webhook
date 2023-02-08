da3fb7e823e26bf879f27076ee623ffdd1800ad3
trodevel <trodevel@gmail.com>
2023-02-08 13:48:04 +0100

    added handler

--- test.py
+++ test.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def hello():
     return 'Webhooks with Python'
 
+@app.route('/githubIssue',methods=['POST'])
+def githubIssue():
+    data = request.json
+    print(f'Issue {data["issue"]["title"]} {data["action"]}')
+    print(f'{data["issue"]["body"]}')
+    print(f'{data["issue"]["url"]}')
+    return data
+
 if __name__ == '__main__':
     app.run(debug=True)
