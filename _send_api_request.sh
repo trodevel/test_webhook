ebe779d72858ce880c25a7a185e3c49f3cd991ab
trodevel <trodevel@gmail.com>
2023-02-09 12:34:59 +0100

    added flag to ignore self-signed certificate

--- _send_api_request.sh
+++ _send_api_request.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -H "Content-type: application/json" -H "Accept: application/json" -X${TYPE} "$API/${REQ}" -d $DATA
+curl -H "Content-type: application/json" -H "Accept: application/json" -X${TYPE} "$API/${REQ}" -d $DATA -k
