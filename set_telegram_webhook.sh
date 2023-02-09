883c482c17d0b475d0297a7648bd7e155a583ee3
trodevel <trodevel@gmail.com>
2023-02-09 14:58:51 +0100

    added param CERT

--- set_telegram_webhook.sh
+++ set_telegram_webhook.sh
@@ -2,11 +2,19 @@
 
 HOST_URL=$1
 HOST_PORT=$2
+CERT=$3
 
 [[ -z $HOST_URL ]]  && echo "ERROR: HOST_URL is not given" && exit 1
 [[ -z $HOST_PORT ]] && echo "ERROR: HOST_PORT is not given" && exit 1
 [[ -z $BOT_TOKEN ]] && echo "ERROR: BOT_TOKEN is not found in evironment" && exit 1
 
-API="https://api.telegram.org/bot${BOT_TOKEN}/setWebhook?url=${HOST_URL}:${HOST_PORT}"
+API="https://api.telegram.org/bot${BOT_TOKEN}/setWebhook"
 
-curl "$API"
+P1="-F url=${HOST_URL}:${HOST_PORT}"
+
+if [[ -n $CERT ]]
+then
+    P2="-F certificate=@$CERT"
+fi
+
+curl -v $P1 $P2 "$API"
